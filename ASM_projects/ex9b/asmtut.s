21c20d278b9bf9fcce8bef84593373308293614b
Alex Coleman <alexjcoleman@gmail.com>
2020-05-26 21:14:26 +0100

    added branch line to correct ex9b script

--- ASM_projects/ex9b/asmtut.s
+++ ASM_projects/ex9b/asmtut.s
@@ -1,15 +1,16 @@
 .global _start
 
 @ <instruction> <Desti>, <Operand>, <Operand>, <Operand>
-@ TEQ (test equivalence bits) performs an ORR on bits and updates the
+@ TEQ (test equivalence bits) performs an EOR on bits and updates the
 @ Zero flag CPSR
 
 _start:
-  MOV R1, #9 @ 1001
-  MOV R2, #8 @ 1000
+  MOV R1, #8 @ 1000
+  MOV R2, #9 @ 1001
   TEQ R1, R2 @ compare values settings flags
   BEQ _bit_set @ if set jump to bit_set (zero flag set)
   MOV R0, #1 @ set output to false
+  B end @ branch to end after this line
 
 _bit_set:
   MOV R0, #0 @ set to true
